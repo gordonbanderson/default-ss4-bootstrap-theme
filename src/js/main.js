c0e55f822b944fa9e6af99f670fa7ff79ca63f20
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-09-09 23:38:14 +0100

    FIX: Add render tweets

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,14 @@
 // Define globally exposed module objects
 /* eslint-disable */
 
-//import flexslider from './thirdparty/jquery.flexslider';
 import loadtwitter from './thirdparty/loadtwitter';
+import rendertweets from './thirdparty/rendertweets';
 import initialiser from './components/initialiser';
 
 import style from '../scss/main.scss';
 
-//flexslider();
 loadtwitter();
+rendertweets();
 initialiser();
 
 
