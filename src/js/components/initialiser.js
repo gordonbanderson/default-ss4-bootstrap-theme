58abbe87e8716314bd986bb59436a3de5d93e368
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-09-12 00:26:59 +0100

    WIP

--- src/js/components/initialiser.js
+++ src/js/components/initialiser.js
@@ -3,9 +3,20 @@ import $ from 'jquery';
 /* eslint-enable */
 
 export default function () {
+
+    // timeago
     $(document).ready(function() {
         $("time.timeago").timeago();
     });
+
+
+    // slider
+    $('.flexslider').flexslider({
+        animation: "slide",
+        prevText: "<",
+        nextText: ">"
+    });
+
 }
 
 
