ff1b5f0e18d89290940560b3f1c3492689c4a6eb
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-09-13 19:07:13 +0100

    MINOR: Commented out cookie consent

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,7 @@ import loadtwitter from './thirdparty/loadtwitter';
 import rendertweets from './thirdparty/rendertweets';
 import lazysizes from './components/lazysizes';
 import initialiser from './components/initialiser';
-import cookieconsent from '../../../../vendor/bramdeleeuw/cookieconsent/javascript/src/cookieconsent.js';
+//import cookieconsent from '../../../../vendor/bramdeleeuw/cookieconsent/javascript/src/cookieconsent.js';
 import cookieconsentpopup from './components/cookieconsentpopup';
 
 import style from '../scss/main.scss';
@@ -13,7 +13,7 @@ lazysizes();
 loadtwitter();
 rendertweets();
 initialiser();
-cookieconsent();
+//cookieconsent();
 cookieconsentpopup();
 
 
