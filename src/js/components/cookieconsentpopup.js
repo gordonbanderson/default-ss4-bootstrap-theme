ce8ad07ace258433d9abf596783c4b1a406453c5
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-09-13 19:06:31 +0100

    MINOR: Debug

--- src/js/components/cookieconsentpopup.js
+++ src/js/components/cookieconsentpopup.js
@@ -4,8 +4,12 @@ import CookieConsent from '../../../../../vendor/bramdeleeuw/cookieconsent/javas
 /* eslint-enable */
 
 export default function () {
+    console.log('Cookie consent T1');
     const consent = new CookieConsent();
     if (!consent.isSet()) {
+        console.log('Cookie consent T2');
         $('#cookieConsentModal').modal();
+        alert('show cookie modal');
     }
+    console.log('Cookie consent T3');
 }
