3a018fabe1e36b4e0c3f9713d420f12e8c492234
Gordon Anderson <gordon.b.anderson@gmail.com>
2019-06-24 19:50:29 +0100

    WIP: Trying to add photoswipe to the pipeline

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,14 @@ import lazysizes from './components/lazysizes';
 import initialiser from './components/initialiser';
 import cookieconsentpopup from './components/cookieconsentpopup';
 
+// @todo Is there a better way to reference JS installed by node?  The ~/name syntax did not seem to work as with css
+// **** THIS IS NOT A COMPONENT **** <<<< that's the issue
+import photoswipe from "../../node_modules/photoswipe/dist/photoswipe.js";
+import photoswipeui from "../../node_modules/photoswipe/dist/photoswipe-ui-default";
+
+import flickrphotoswipe from './thirdparty/flickrswipe';
+
+
 import style from '../scss/main.scss';
 
 lazysizes();
@@ -14,6 +22,9 @@ loadtwitter();
 rendertweets();
 initialiser();
 cookieconsentpopup();
+photoswipe();
+photoswipeui();
+flickrphotoswipe();
 
 
 
