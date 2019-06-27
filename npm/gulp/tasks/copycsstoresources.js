3733f3b2b775e511696d86ff29418fbc6dcec09e
Gordon Anderson <gordon.b.anderson@gmail.com>
2019-06-27 22:53:35 +0100

    MINOR: Remove legacy gulp build

--- npm/gulp/tasks/copycsstoresources.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function(gulp, $, config, messages) {
-    gulp.task('copycsstoresources', function() {
-        //gulp.src('src/css/**/*.css')
-        return gulp.src('../css/dist/**/*.css')
-			.pipe(gulp.dest('../../../public/resources/themes/suilven_default/css/'))
-    });
-};
--- npm/gulp/tasks/copyjstoresources.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function(gulp, $, config, messages) {
-    gulp.task('copyjstoresources', function() {
-        return gulp.src('../javascript/dist/**/*.js')
-			.pipe(gulp.dest('../../../public/resources/themes/suilven_default/javascript/'))
-    });
-};
